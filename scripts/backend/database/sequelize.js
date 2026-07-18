2e868f5d2592ee623152be3ea0a65ab8fdb165af
Goncalo <goncalomachferreira@gmail.com>
2026-07-18 22:59:56 +0100

    dotenv

--- scripts/backend/database/sequelize.js
+++ scripts/backend/database/sequelize.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
 
 const isProduction = process.env.NODE_ENV === "production";
--- scripts/server.js
+++ scripts/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const path = require("path");
 const cors = require("cors");
 const express = require("express");
